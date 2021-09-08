f875801a8f2b0beba821d4adab876d4d6eda1f78
Jesus <jcrisquez@us.es>
2021-09-08 19:19:39 +0200

    [ ADD ]  New examples

--- scripts/Scripts_Teoria/Tema_Repaso.py
+++ scripts/Scripts_Teoria/Tema_Repaso.py
@@ -7,12 +7,17 @@ Created on Fri Sep  3 19:03:13 2021
 """
 
 import itertools
+import math
 
 Omega = set( [1,2,3,4,5,6,7,8,9,10] )
 A = set( [1,2,3,4,5] )
 B = set( [1,4,5] )
 C = set( [4,6,7,8] )
 D = set( [ "VERDE", "AMARILLO", "AZUL", "ROSA", "NARANJA", "ROJO" ] )
+
+E = set( [ "B", "Y", "T", "E" ] )
+
+E = set( range(1, 51) )
 
 A & B   # intersección (&: and/y)
 A | B   # unión (|: or/o)
@@ -40,5 +45,39 @@ Sol_P1 = [ val for val in itertools.permutations( "ABCDEFGHIJ", 4 ) ]
 
 # Problema 2
 Sol_P2 = [ val for val in itertools.combinations( D , 3 ) ]
+
+
+# Problema 3
+Sol_P3 = [ val for val in itertools.permutations( E , 4 ) ]
+
+
+# Problema 4
+Sol_P4 = [ val for val in itertools.permutations( set( range(1, 51) ) , 2 ) ]
+
+
+# Problema 5
+Sol_P5 = [ val for val in itertools.combinations( set( range(1, 12) ) , 5 ) ]
+
+# Problema 6
+Sol_P6 = math.factorial( 13 )
+
+
+
+# Problema 10
+Sol_P10 = [ val for val in itertools.combinations_with_replacement( set( [ "AMARILLO", "AZUL", "NARANJA", "ROJO" ] ) , 2 ) ]
+
+
+
+# Problema 12
+Sol_P12 = []
+Sol_P12_1 = [ val for val in itertools.combinations( set( range(1,11) ) , 3 ) ]
+Sol_P12_2 = [ val for val in itertools.combinations( set( [ "E1", "E2", "E3", "E4", "E5", "E6" ] ) , 2 ) ]
+
+
+for val_1 in Sol_P12_1:
+    for val_2 in Sol_P12_2:
+        Sol_P12.append( list( val_1 ) + list( val_2 )  )
+
+
 
 
